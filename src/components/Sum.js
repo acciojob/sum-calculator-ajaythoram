c7e652e579314550d9cfcc556361ebafd32efbe6
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-06-26 06:54:51 +0530

    submitting code-Mon Jun 26 2023 06:54:47 GMT+0530 (India Standard Time)

--- src/components/Sum.js
+++ src/components/Sum.js
@@ -4,6 +4,7 @@ const Sum = ()=>{
     let [sum,setSum] = useState(0);
     return(
         <div>
+            <h1>Sum Calculator</h1>
  <input type="number" onChange={(e)=>(setSum(parseInt(sum) + parseInt(e.target.value)))} />
   <p>Sum: {sum}</p>
         </div>
